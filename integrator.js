f32ec3718b019aef1b9fddc61a9abb4c446d3c2c
Andre Sato <andre.sato@arctouch.com>
2017-03-10 17:03:33 -0300

    Improve integrator with conversation flow

--- integrator.js
+++ integrator.js
@@ -14,17 +14,17 @@ var oauthClient = restify.createStringClient({
 
 var botClient = null;
 var token = "";
-var conversationId = "";
-var waterMark = 0;
+var threadIds = [];
+var conversations = [];
 
 oauthClient.post(registrationPortal, function (err, req, res, obj){
 		console.log("Token %j", obj);
 		token = obj.replace(/"/g,"");
-		getConversationId();
+		startListeningStream();
 	}
 );
 
-function getConversationId() {
+function getConversationId(user, threadId, content, onReceiving) {
 	botClient = restify.createJsonClient({
 		url: process.env.MICROSOFT_DIRECT_LINE_URL,
 		headers: {
@@ -35,8 +35,10 @@ function getConversationId() {
 	botClient.post(apiConversations, function(err, req, res, obj) {
 		if(!err){
 			console.log("Conversation ID: %j", obj);
-			conversationId = obj.conversationId;
-			startListeningStream();
+			getMessages(0, obj.conversationId, threadId);
+			if(onReceiving) {
+				onReceiving(user, threadId, content, obj.conversationId);
+			};
 		} else {
 			console.log("Error when getting conversationId: %s", err);
 		}
@@ -44,42 +46,45 @@ function getConversationId() {
 }
 
 
-function sendMessageFromUser(user, message, threadId) {
+function sendMessageFromUser(user, message, threadId, conversationId) {
 	botClient.post(apiConversations + "/" + conversationId + "/messages", 
 		{
 			text:message,
 			from:user
 		}, function(err,req,res) {
-			console.log("Message sent!");
-			if(!err) {
-				waterMark ++;
-				getMessages(waterMark, threadId);
-			} else {
+			console.log("Message sent to bot! ConversationID[%s]", conversationId);
+			if(err) {
 				console.log("Error when sending: %s", err);
 			}
 		});
 }
 
-function getMessages(waterMarkNumber, threadId) {
+function getMessages(waterMarkNumber, conversationId, threadId) {
 	console.log("getting message from %s", waterMarkNumber);
 	botClient.get(apiConversations + "/" + conversationId + "/messages?watermark=" + waterMarkNumber, 
 		function(err, req, res, obj) {
-			console.log("Message received!");
+			console.log("Message received from bot!");
 			if(!err) {
 				console.log("bot response: %j", obj);
-				if(obj.messages[0] == null) {
-					setTimeout(function () {
-						getMessages(waterMarkNumber, threadId);
-					}, 500);
-				} else {
-					waterMark = parseInt(obj.watermark);
+				if(obj.messages[0] != null && 
+					obj.watermark && 
+					waterMarkNumber < obj.watermark &&
+					obj.messages[0].from == "hottie") {
 					sendMessageToFlowdock(obj.messages[0].text, threadId);
 				}
 			} else {
 				console.log(err);
 			}
+
+			setTimeout(function () {
+				if(obj.watermark) {
+					waterMarkNumber = obj.watermark;
+				}
+				getMessages(waterMarkNumber, conversationId, threadId);
+			}, 500);
 		});
 }
+
 function startListeningStream() {
 	var stream = new EventSource(process.env.FLOWDOCK_ENDPOINT);
 	stream.onmessage = function(event) {
@@ -90,7 +95,24 @@ function startListeningStream() {
 		if(message.event == 'message'){
 			console.log("sending message from %s as %s", message.user, message.content);
 			if(!message.content.startsWith('[bot]')) {
-				sendMessageFromUser(message.user, message.content, message.thread_id);
+				var conversation = conversations.find(function(con) { return con.threadId == message.thread_id});
+				if(!conversation){
+					console.log("Initializing new conversation as threadId[%s] is not inside %j", message.thread_id, conversations)
+					getConversationId(message.user, message.thread_id, message.content, 
+						function(user, threadId, content, conversationId) {
+							console.log("get conversation call back: user[%s], threadId[%s], content[%s], conversationId[%s]", user, threadId, content, conversationId)
+							conversation = {
+								threadId:threadId,
+								conversationId:conversationId
+							};
+							console.log("Adding conversation: %j", conversation)
+							conversations.push(conversation);
+							sendMessageFromUser(user, content, threadId, conversationId);
+						});
+				} else {
+					console.log("Continue conversation as threadId[%s] is inside %j", message.thread_id, conversations)
+					sendMessageFromUser(message.user, message.content, message.thread_id, conversation.conversationId);
+				}
 			}
 		}
 	};
